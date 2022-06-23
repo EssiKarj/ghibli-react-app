f939a76765ac8a7a3e427ed7c8786648bd33d683
EssiKarj <essi-karj@icloud.com>
2022-06-23 09:58:29 +0100

    Fixing for deployment

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -2,16 +2,17 @@ import React, { useEffect } from 'react'
 import MovieDisplay from '../components/MovieDisplay'
 
 const Overview = ({ movieData, setMovieData, setMovie }) => {
-  //API request to execute on first render of the page and store first fove movies to movieData state
+  //API request to execute on first render of the page and store first five movies to movieData state
   useEffect(() => {
     const getData = async () => {
+      if (!!movieData.length) return
       const response = await fetch('https://ghibliapi.herokuapp.com/films')
       const data = await response.json()
       setMovieData(data.slice(0, 5))
     }
 
     getData()
-  }, [])
+  }, [setMovieData, movieData])
 
   return (
     <div className='main-container'>
